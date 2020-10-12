8195cb319d69b2ec34a55e7a567a57cc7356b66b
vikuviku6666 <53383503+vikuviku6666@users.noreply.github.com>
2020-10-12 17:01:00 +0200

    Update App.tsx
    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.scss';
 
 function App() {
+  let name: string = "karl";
   return (
     <div className="App">
-    Learn React Yes we are learning
+    Learn React Yes we are learning with {name}
     </div>
   );
 }
 
 export default App;
+
+
+      
